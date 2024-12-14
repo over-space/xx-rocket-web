ddf66925b489af0dcb12b049c022354c4f9c96dd
lifang <over.li@flip.fun>
2024-12-14 20:04:32 +0800

    init rocket-web

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-use employee::employee::{none, get_employee, list_employee};
-
+use common::controller::index;
+use employee::controller::{get_employee, list_employee, none};
 
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index])
-    .mount("/employee", routes![none, get_employee, list_employee])
+        .mount("/", routes![index])
+        .mount("/employee", routes![none, get_employee, list_employee])
 }

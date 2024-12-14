6418bc9a28990eb903b5db182fa2698732a408ac
lifang <over.li@flip.fun>
2024-12-14 22:29:53 +0800

    dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use common::controller::index;
 use employee::controller::{get_employee, list_employee, none};
 
@@ -6,6 +8,11 @@ extern crate rocket;
 
 #[launch]
 fn rocket() -> _ {
+    dotenv::dotenv().ok();
+    for (k, v) in env::vars() {
+        println!("k = {}, v = {}", k, v)
+    }
+
     rocket::build()
         .mount("/", routes![index])
         .mount("/employee", routes![none, get_employee, list_employee])
